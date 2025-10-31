006fdd145691f1b61fc4d4f92b574615638c20a2
Justyna-Bulach-student <263902@student.pwr.edu.pl>
2025-10-31 21:27:15 +0100

    Update app.py

--- back-end/app.py
+++ back-end/app.py
@@ -11,7 +11,7 @@ api = Api(app)
 app.config['JWT_SECRET_KEY'] = JWT_KEY
 jwt = JWTManager(app)
 
-# sprawdzam działanie GitHub Actions dla frontendu v2 (sprawdzenie czy będzie kontunacja pipeline'u)
+# sprawdzam działanie GitHub Actions dla frontendu v3 (sprawdzenie czy będzie kontunacja pipeline'u)
 
 init_db()
 
