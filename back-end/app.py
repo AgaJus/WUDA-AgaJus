cc6509b8bdc09b482e90bb03f7f21a9ae7db4111
Justyna-Bulach-student <263902@student.pwr.edu.pl>
2025-10-31 23:49:06 +0100

    cicd test

--- back-end/app.py
+++ back-end/app.py
@@ -11,7 +11,7 @@ api = Api(app)
 app.config['JWT_SECRET_KEY'] = JWT_KEY
 jwt = JWTManager(app)
 
-# sprawdzam działanie GitHub Actions dla frontendu v15 (sprawdzenie czy będzie kontunacja pipeline'u)
+# sprawdzam działanie GitHub Actions dla frontendu v16 (sprawdzenie czy będzie kontunacja pipeline'u)
 
 init_db()
 
